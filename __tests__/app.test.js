b7b705668a7cb9e5d80893d3667c44d8d83d791e
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-11-02 09:58:25 -0700

    completed first assigned test.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,26 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('example test - you should probably update me', () => {
-  it('home route should return hello world', async () => {
-    const resp = await request(app).get('/');
-    expect(resp.text).toEqual('hello world!');
+const { zodiacs } = require('../lib/zodiac-data');
+
+describe('zodiac routes', () => {
+  it('/zodiacs should return an array of zodiacs', async () => {
+    const res = await request(app).get('/zodiacs');
+    const expected = zodiacs.map((zodiac) => {
+      return { id: zodiac.id, name: zodiac.name };
+    });
+    expect(res.body).toEqual(expected);
   });
 });
+
+
+
+
+
+
+// describe('example test - you should probably update me', () => {
+//   it('home route should return hello world', async () => {
+//     const resp = await request(app).get('/');
+//     expect(resp.text).toEqual('hello world!');
+//   });
+// });
